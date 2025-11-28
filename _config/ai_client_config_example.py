80ad42ab04ff5f45aa345a48b36b89928c575420
Sleepy <sleepysoft@gmail.com>
2025-11-28 15:53:03 +0800

    Update config example.

--- _config/ai_client_config_example.py
+++ _config/ai_client_config_example.py
@@ -1,6 +1,7 @@
-# --------------------------------------------------------------------
+# --------------------------------------------------------------------------------
 # Python is config. We don't need a json file and load it, analyze it.
-# --------------------------------------------------------------------
+# Rename ai_client_config_example.py to ai_client_config.py to enable this config.
+# --------------------------------------------------------------------------------
 
 from typing import List
 
@@ -18,29 +19,34 @@ def build_ai_clients() -> List[BaseAIClient]:
     # - Use high balance account's token
     # - It is considered available by default.
     # - Once lower value token is available, client manage will not suggest this client.
-    # - The Initialize is in environment variant "SILICON_API_KEY"
+    # - The Initialize is in environment variant "SILICON_API_KEY", or set token here.
     # -------------------------------------------------
 
     sf_api_default = create_siliconflow_client('A valid token')
     sf_client_default = StandardOpenAIClient(
-        'SiliconFlow Client Default',
-        sf_api_default,
-        CLIENT_PRIORITY_EXPENSIVE,
+        name='SiliconFlow Client Default',
+        openai_api=sf_api_default,
+        priority=CLIENT_PRIORITY_EXPENSIVE,
+        group_id='silicon flow',
         default_available=True,
         balance_config={ 'hard_threshold': 10 }
     )
+    # Because there's no rotator to update its balance. Just set a good value to make it health.
+    sf_client_default.update_balance(100)
 
     # -------- The token-rotation silicon flow client --------
     # - Use low-value token list.
     # - Init multiple clients, because sf will respond 504 when switching to another key.
+    # - Note that there may be session limitation per ip, so we should put them in a same group and set group limit.
     # - Initialize token set to empty.
     # --------------------------------------------------------
 
     sf_api_a = create_siliconflow_client('invalid')
     sf_client_a = OuterTokenRotatingOpenAIClient(
-        'SiliconFlow Client A',
-        sf_api_a,
-        CLIENT_PRIORITY_NORMAL,
+        name='SiliconFlow Client A',
+        openai_api=sf_api_a,
+        priority=CLIENT_PRIORITY_NORMAL,
+        group_id='silicon flow',
         balance_config={ 'hard_threshold': 0.1 }
     )
     sf_rotator_a = SiliconFlowServiceRotator(
@@ -54,9 +60,10 @@ def build_ai_clients() -> List[BaseAIClient]:
 
     sf_api_b = create_siliconflow_client('invalid')
     sf_client_b = OuterTokenRotatingOpenAIClient(
-        'SiliconFlow Client B',
-        sf_api_b,
-        CLIENT_PRIORITY_NORMAL,
+        name='SiliconFlow Client B',
+        openai_api=sf_api_b,
+        priority=CLIENT_PRIORITY_NORMAL,
+        group_id='silicon flow',
         balance_config={ 'hard_threshold': 0.1 }
     )
     sf_rotator_b = SiliconFlowServiceRotator(
@@ -78,6 +85,7 @@ def build_ai_clients() -> List[BaseAIClient]:
 
     # Modelscope: A total of 2000 free API-Inference calls per day, with a limit of 500 calls per single model
     #             However, only the following three 400B+ models are actually available.
+    #             There should be a more strict limitation. So just rotate the models and keys. Do not build too many clients.
 
     ms_models = [
         'deepseek-ai/DeepSeek-R1-0528',
@@ -85,15 +93,22 @@ def build_ai_clients() -> List[BaseAIClient]:
         'Qwen/Qwen3-Coder-480B-A35B-Instruct'
     ]
 
-    # --------------------------------------------------------
+    ms_tokens = [
+        'Token1',
+        'Token2',
+        'Token3'
+    ]
 
     ms_api = create_modelscope_client('ms-xxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
     ms_client = SelfRotatingOpenAIClient(
-        f'ModelScope Client 1',
-        ms_api, CLIENT_PRIORITY_FREEBIE,
+        name=f'ModelScope Client',
+        openai_api=ms_api,
+        priority=CLIENT_PRIORITY_FREEBIE,
+        group_id='silicon flow',
         default_available=True
     )
-    ms_client.set_rotation_models(ms_models)
+    ms_client.set_rotation_models(ms_models, rotate_per_times=5)
+    ms_client.set_rotation_tokens(ms_tokens, rotate_per_times=15)   # Models (3) x Rotate Times (5) => Rotate Token (15)
     ms_client.set_usage_constraints(max_tokens=495, period_days=1, target_metric='request_count')
 
     # --------------------------------------------------------
